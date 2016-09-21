60a12c288c41fbea558bff0b5823f86008631da5
symbol <symbol_for_success@hotmail.com>
2016-09-22 10:42:49 +1200

    start

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTablesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularTablesApp
+ */
+angular.module('angularTablesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTablesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularTablesApp
+ */
+angular.module('angularTablesApp')
+  .controller('MainCtrl', function () {
+    
+  });
